6570fec5cc7bd018b34222c670a51f6edd9badc9
M1hono <xche0557@uni.sydney.edu.au>
2026-04-17 06:21:16 +1000

    fix: keep example bootstrap out of core mod

--- src/main/java/org/pickaid/pibrary/Pibrary.java
+++ src/main/java/org/pickaid/pibrary/Pibrary.java
@@ -3,7 +3,6 @@ package org.pickaid.pibrary;
 import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
-import org.pickaid.pibrary.dev.example.CounterLivingServices;
 import org.pickaid.pibrary.runtime.service.PiLivingServiceDescriptors;
 import org.pickaid.pibrary.runtime.sync.PiLivingSyncMessages;
 import org.slf4j.Logger;
@@ -16,7 +15,6 @@ public final class Pibrary {
     public Pibrary() {
         LOGGER.info("Initializing {}", MOD_ID);
         PiLivingServiceDescriptors.bootstrap();
-        CounterLivingServices.register();
         PiLivingSyncMessages.bootstrap();
     }
 
